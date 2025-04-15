647ac308f9e6c735ea1ef715a0153777a3be7500
Xander Swan <xanderswan32@gmail.com>
2025-04-14 22:44:25 -0400

    how

--- bin/CMakeFiles/luajit.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/luajit"
-  "lib/luajit/src/libluajit.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/luajit.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
